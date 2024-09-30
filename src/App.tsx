ec956df60d5c528171deea2696bea8e40c8fc304
Eega Somasekhara Reddy <somasekhareega@gmail.com>
2024-10-01 01:24:45 +0530

    added github link

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,17 @@ const App = () => {
   return (
     <div className=" relative">
       <MealContext.Provider value={{ mealDetails, setMealDetails }}>
-        <div className=" absolute top-0 w-full shadow-md px-4 py-2 bg-white z-10 ">
-          Food Explorer
+        <div className=" flex flex-row items-center justify-between absolute top-0 w-full shadow-md px-4 py-2 bg-white z-10 ">
+          <span className=" text-xl font-medium">Food Explorer</span>
+          <a
+            href="https://github.com/somu050600/node-bites"
+            target="_blank"
+            rel="noopener noreferrer"
+            className=" sm:bg-slate-200 rounded-lg p-1 sm:px-3 sm:py-1 "
+          >
+            <i className="fa-brands fa-square-github fa-xl sm:fa-lg mr-1" />
+            <span className="hidden sm:inline">GitHub</span>
+          </a>
         </div>
         <ReactFlowProvider>
           <NodeGraph />
